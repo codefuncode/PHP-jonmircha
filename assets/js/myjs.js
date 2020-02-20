243bb842b76074385af2bb68b733ba21b31eed02
Carlos Alemán <alemandiazcarlos@tuta.io>
2020-02-20 14:57:43 -0400

    ok

---
+++ assets/js/myjs.js
@@ -0,0 +1,23 @@
+//
+//
+// -----------------------------------------
+// Funciones para interactividad del usuario
+//
+// Para encontrar el código emplead en alguna de 
+// las paginas puede usar la función control + f 
+// de su editor de texto preferido.  
+// -----------------------------------------
+// 
+// 
+// --------------------------------------------
+// -----------------
+//  Code : # 0000003
+// -----------------
+// Codgo para la pagina JS/paginas/000003.php
+function alertModulo_3() {
+	var btn_alert_modulo_3 = document.getElementById('btn_alert_modulo_3');
+	btn_alert_modulo_3.onclick = function () {
+		alert("Esto es una alerta ");
+	};
+}
+//  ------------------------------------------------------------------------
