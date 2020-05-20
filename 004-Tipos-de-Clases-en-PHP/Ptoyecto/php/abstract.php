db1379af2f6fa345c81b24713834583679e1e694
codefuncode <codefuncode@protonmail.com>
2020-05-20 13:20:04 -0400

    Actualizacion de seguimiento

--- 004-Tipos-de-Clases-en-PHP/Ptoyecto/php/abstract.php
+++ 004-Tipos-de-Clases-en-PHP/Ptoyecto/php/abstract.php
@@ -3,8 +3,90 @@
 abstract class ClassNameAbstract
 {
 
-  public $atributo_abstracto;
+    public $atributo_abstracto;
 
-  public function MetodoAbstracto();
+    public function MetodoAbstracto();
 
 }
+// ==================================================
+//  w3 code example
+abstract class ParentClass
+{
+    abstract public function someMethod1();
+    abstract public function someMethod2($name, $color);
+    abstract public function someMethod3(): string;
+}
+// ====================================================
+abstract class Car
+{
+    public $name;
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+    abstract public function intro(): string;
+}
+
+// Child classes
+class Audi extends Car
+{
+    public function intro(): string
+    {
+        return "Choose German quality! I'm an $this->name!";
+    }
+}
+
+class Volvo extends Car
+{
+    public function intro(): string
+    {
+        return "Proud to be Swedish! I'm a $this->name!";
+    }
+}
+
+class Citroen extends Car
+{
+    public function intro(): string
+    {
+        return "French extravagance! I'm a $this->name!";
+    }
+}
+
+// Create objects from the child classes
+$audi = new audi("Audi");
+echo $audi->intro();
+echo "<br>";
+
+$volvo = new volvo("Volvo");
+echo $volvo->intro();
+echo "<br>";
+
+$citroen = new citroen("Citroen");
+echo $citroen->intro();
+
+// =================================================
+abstract class ParentClass
+{
+    // Abstract method with an argument
+    abstract protected function prefixName($name);
+}
+
+class ChildClass extends ParentClass
+{
+    public function prefixName($name)
+    {
+        if ($name == "John Doe") {
+            $prefix = "Mr.";
+        } elseif ($name == "Jane Doe") {
+            $prefix = "Mrs.";
+        } else {
+            $prefix = "";
+        }
+        return "{$prefix} {$name}";
+    }
+}
+
+$class = new ChildClass;
+echo $class->prefixName("John Doe");
+echo "<br>";
+echo $class->prefixName("Jane Doe");
